495e59d25e8d9f2bb2be729a7750d016d6147cd3
qui <qui@outlook.com>
2014-06-16 02:16:48 -0400

    tab

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ var express = require('express'),
     expressValidator = require('express-validator'),
     sass = require('node-sass'),
      _ = require('underscore'),
-     redis_client = require('redis-client-manager'),
     NodeCache = require('node-cache');
 
 // Configuration, default - local mode
